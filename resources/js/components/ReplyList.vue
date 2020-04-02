d1aa1406ba045d14ae27b7922fea6460c1eee031
Paul Santos <paulmarlonsantos@gmail.com>
2020-04-03 05:45:39 +0800

    wip

--- resources/js/components/ReplyList.vue
+++ resources/js/components/ReplyList.vue
@@ -47,7 +47,7 @@
 
         <transition
             enter-active-class="transition duration-300 ease-in-out"
-            leave-active-class="transition duration-300 ease-in-out"
+            leave-active-class="transition duration-100 ease-in-out"
             enter-class="opacity-0"
             enter-to-class="opacity-100"
             leave-class="opacity-100"

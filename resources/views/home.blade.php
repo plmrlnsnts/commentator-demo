41cd7fd1218832b241a3f02d0794a4bb4f075ed4
Paul Santos <paulmarlonsantos@gmail.com>
2020-04-03 01:29:52 +0800

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 <div>
     @guest
-        <form class="flex items-center justify-center py-24" action="{{ route('login') }}" method="POST">
+        <form class="text-center py-24" action="{{ route('login') }}" method="POST">
             @csrf
             <input type="hidden" name="email" value="{{ $user->email }}">
             <input type="hidden" name="password" value="password">
-            <button type="submit" class="bg-yellow-500 rounded font-medium px-4 py-3 focus:outline-none">Click to view discussions</button>
+            <div class="mb-8"><img src="{{ asset('placeholder.svg') }}" class="inline-block w-32" alt=""></div>
+            <button type="submit" class="border rounded font-medium px-4 py-3 focus:outline-none">Login to view discussions</button>
         </form>
     @else
         <comment-list :commentable='@json($commentable)' :user='@json(Auth::user())'></comment-list>

8c65a981b92bf87133f2966e92d7be8de6dd931e
Paul Santos <paulmarlonsantos@gmail.com>
2020-04-03 00:12:03 +0800

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,12 +8,12 @@
     <meta name="description" content="A Laravel package for adding comments to your model.">
     <title>Laravel Commentator</title>
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="font-body text-gray-900 antialiased">
     <div id="app">

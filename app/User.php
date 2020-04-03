4f0dfcdaaab5bb6a770a490ce3dd50a150801fdc
Paul Santos <paulmarlonsantos@gmail.com>
2020-04-04 05:14:09 +0800

    wip

--- app/User.php
+++ app/User.php
@@ -37,10 +37,20 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
     protected $appends = [
         'avatar'
     ];
 
+    /**
+     * The avatar url for this user.
+     *
+     * @return string
+     */
     public function getAvatarAttribute()
     {
         return 'https://api.adorable.io/avatars/' . $this->email . '.png';
